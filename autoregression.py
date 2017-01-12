6031ecb833d996b11616506b64949a795a7bd94c
Egil <egil.moller@freecode.no>
2017-01-12 20:42:13 +0100

    deleted all the comments

--- autoregression.py
+++ autoregression.py
@@ -13,19 +13,6 @@
 # limitations under the License.
 # ==============================================================================
 
-"""Builds the MNIST network.
-
-Implements the inference/loss/training pattern for model building.
-
-1. inference() - Builds the model as far as is required for running the network
-forward to make predictions.
-2. loss() - Adds to the inference model the layers required to generate loss.
-3. training() - Adds to the loss model the Ops required to generate and
-apply gradients.
-
-This file is used by the various "fully_connected_*.py" files and not meant to
-be run.
-"""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -34,7 +21,6 @@ import math
 
 import tensorflow as tf
 
-# The MNIST dataset has 10 classes, representing the digits 0 through 9.
 NUM_CLASSES = 10
 
 # The MNIST images are always 28x28 pixels.
@@ -43,15 +29,6 @@ IMAGE_PIXELS = 16 # IMAGE_SIZE * IMAGE_SIZE
 
 
 def inference(images, hidden1_units):
-  """Build the MNIST model up to where it may be used for inference.
-
-  Args:
-    images: Images placeholder, from inputs().
-    hidden1_units: Size of the first hidden layer.
-
-  Returns:
-    softmax_linear: Output tensor with the computed logits.
-  """
   levels = 3
   net = images
   prev_size = IMAGE_PIXELS
@@ -84,57 +61,16 @@ def inference(images, hidden1_units):
   return net
 
 def loss(logits, labels):
-  """Calculates the loss from the logits and the labels.
-
-  Args:
-    logits: Logits tensor, float - [batch_size, IMAGE_PIXELS].
-    labels: Labels tensor, float - [batch_size, IMAGE_PIXELS].
-
-  Returns:
-    loss: Loss tensor of type float.
-  """
-
   return tf.reduce_mean(tf.squared_difference(logits, labels), name='mse')
 
 
 def training(loss, learning_rate):
-  """Sets up the training Ops.
-
-  Creates a summarizer to track the loss over time in TensorBoard.
-
-  Creates an optimizer and applies the gradients to all trainable variables.
-
-  The Op returned by this function is what must be passed to the
-  `sess.run()` call to cause the model to train.
-
-  Args:
-    loss: Loss tensor, from loss().
-    learning_rate: The learning rate to use for gradient descent.
-
-  Returns:
-    train_op: The Op for training.
-  """
-  # Add a scalar summary for the snapshot loss.
   tf.summary.scalar('loss', loss)
-  # Create the gradient descent optimizer with the given learning rate.
   optimizer = tf.train.GradientDescentOptimizer(learning_rate)
-  # Create a variable to track the global step.
   global_step = tf.Variable(0, name='global_step', trainable=False)
-  # Use the optimizer to apply the gradients that minimize the loss
-  # (and also increment the global step counter) as a single training step.
   train_op = optimizer.minimize(loss, global_step=global_step)
   return train_op
 
 
 def evaluation(logits, labels):
-  """Evaluate the quality of the logits at predicting the label.
-
-  Args:
-    logits: Logits tensor, float - [batch_size, IMAGE_PIXELS].
-    labels: Labels tensor, float - [batch_size, IMAGE_PIXELS]
-
-  Returns:
-    A scalar int32 tensor with the number of examples (out of batch_size)
-    that were predicted correctly.
-  """
   return loss(logits, labels)
